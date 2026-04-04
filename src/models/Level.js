2dff9cc6cdc111498a0f43308921db1e5758d168
Solomon <solomonawitte@noreply.com>
2026-04-04 17:03:56 -0500

    fixed db host

--- src/models/Level.js
+++ src/models/Level.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 require('dotenv/config')
 
 const pool = mysql.createPool({
-    host: 'melopepo.xyz',
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 's9_discord_bot_db',
