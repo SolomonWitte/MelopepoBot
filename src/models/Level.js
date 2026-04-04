20a769f8a209810e121fa588dc233075e27a8f0d
Solomon <solomonawitte@noreply.com>
2026-04-04 16:56:45 -0500

    fixed db user

--- src/models/Level.js
+++ src/models/Level.js
@@ -4,7 +4,7 @@ require('dotenv/config')
 
 const pool = mysql.createPool({
     host: 'melopepo.xyz',
-    user: 'botuser',
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 's9_discord_bot_db',
 });
